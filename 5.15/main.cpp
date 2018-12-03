e99ac40af6d15b6108357d6c4dbcb7d60230029f
DESKTOP-PGM3OM5\wkymn <1060775313@qq.com>
2018-12-03 23:39:14 +0800

    11月 十三周 c++作业 段芊芊

---
+++ 5.15/main.cpp
@@ -0,0 +1,10 @@
+#include "GradeBook.h"
+
+int main()
+{
+   GradeBook myGradeBook( "CS101 C++ Programming" );
+
+   myGradeBook.displayMessage();
+   myGradeBook.inputGrades();
+   myGradeBook.displayGradeReport();
+}
